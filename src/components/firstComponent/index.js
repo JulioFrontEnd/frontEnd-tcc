d9bcad73f787a6fe5c8c8a437aa352b15cc33fea
Your Name <you@example.com>
2020-01-16 15:59:44 -0300

    adding component state

--- src/components/firstComponent/index.js
+++ src/components/firstComponent/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
-
-
+import './index.scss';
 export default class Index extends React.Component{
-    render(){
-        return(
 
-            <h1>HELLO WORLD!</h1>
+    state = {
+        color:"red",
+    }
+
+    alterarCor = ()=>{
+        if(this.state.color === 'red'){
+            this.setState({color:"blue"});
+        }else{
+            this.setState({color:"red"});
+        }
+    }
+
+    render(){
+        const color = {
+            color: this.state.color
+        }
+        
+        return(
+            <div className="myFirstDiv">
+                <h1 onClick={this.alterarCor} style={color}>
+                    HELLO WORLD!
+                </h1>
+                <h2>
+                    And hello Brazil!
+                </h2>
+            </div>
         );
     }
 }

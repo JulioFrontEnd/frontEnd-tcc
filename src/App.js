566df3616e2050b12cc9f9874714ec8b538c1bb1
Your Name <you@example.com>
2020-01-16 15:18:46 -0300

    fist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Index from './components/firstComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Index} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
